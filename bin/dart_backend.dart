3beb3459f005e9b15f4e9e7f0555cd0c3af0a221
Rychard Souza <rychard.souza@popcode.com.br>
2023-02-13 18:10:32 -0300

    iniciando server

--- bin/dart_backend.dart
+++ bin/dart_backend.dart
@@ -1,5 +1,14 @@
-import 'package:dart_backend/dart_backend.dart' as dart_backend;
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_backend.calculate()}!');
+void main() async {
+  
+  final server = await shelf_io.serve((request) => Response(200, body: 'OK'), 'localhost', 8080);
+
+  print('Servidor Iniciado ${server.address.host}:${server.port}');
+
+
+
+
+
 }
