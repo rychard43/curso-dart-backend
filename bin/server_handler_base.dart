9802bc94bd76c5214137267e7f77eb90bbc6fd5b
Rychard Souza <rychard.souza@popcode.com.br>
2023-02-18 19:28:56 -0300

    usando metodo post

--- bin/server_handler_base.dart
+++ bin/server_handler_base.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
@@ -16,6 +18,19 @@ class ServerHandlerBase {
     router.get('/query', (Request request) {
       String? dado = request.url.queryParameters['dado'];
       return Response.ok("Olá, dado da rota query: $dado");
+    });
+
+    router.post('/login', (Request request) async {
+      var result = await request.readAsString();
+      Map json = jsonDecode(result);
+      var usuario = json['usuario'];
+      var senha = json['senha'];
+
+      if (usuario == 'admin' && senha == 'admin') {
+        return Response.ok('Bem vindo $usuario');
+      } else {
+        return Response.forbidden('Não tem acesso');
+      }
     });
 
     return router;
